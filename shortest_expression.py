ce5c1f735c8beb9c0f60b988f7a0559f73d3fd0e
eichelb4rt <maurice.wenig@gmail.com>
2024-06-23 00:18:09 +0200

    now updating for less used numbers

--- shortest_expression.py
+++ shortest_expression.py
@@ -15,6 +15,7 @@ class Expression:
     expression_string: str
     length: int
     last_operator: str
+    allowed_numbers_used: set[int]
 
 
 def all_combinations(expression_1: Expression, expression_2: Expression) -> list[Expression]:
@@ -27,14 +28,15 @@ def all_combinations(expression_1: Expression, expression_2: Expression) -> list
         expression_2_str = f"({expression_2.expression_string})" if use_brackets_2 else expression_2.expression_string
         new_expression = f"{expression_1_str} {operator} {expression_2_str}"
         new_result = eval(f"{expression_1.result}{operator}{expression_2.result}")
-        combinations.append(Expression(new_result, new_expression, expression_1.length + expression_2.length, operator))
+        new_allowed_numbers_used = expression_1.allowed_numbers_used.union(expression_2.allowed_numbers_used)
+        combinations.append(Expression(new_result, new_expression, expression_1.length + expression_2.length, operator, new_allowed_numbers_used))
     return combinations
 
 
 def get_shortest_expressions(lower_bound: int, upper_bound: int) -> dict[int, Expression]:
     # length -> expressions of that length
     expressions_of_length: dict[int, list[Expression]] = {}
-    expressions_of_length[1] = [Expression(allowed_number, str(allowed_number), 1, "_") for allowed_number in ALLOWED_NUMBERS]
+    expressions_of_length[1] = [Expression(allowed_number, str(allowed_number), 1, "_", {allowed_number}) for allowed_number in ALLOWED_NUMBERS]
 
     # number -> Expression
     found_expressions: dict[int, Expression] = {}
@@ -47,10 +49,16 @@ def get_shortest_expressions(lower_bound: int, upper_bound: int) -> dict[int, Ex
             for expression_1 in expressions_of_length[length_1]:
                 for expression_2 in expressions_of_length[length_2]:
                     for combination in all_combinations(expression_1, expression_2):
-                        if combination.result >= lower_bound and combination.result <= upper_bound and combination.result not in found_expressions:
-                            expressions_of_length[result_length].append(combination)
-                            found_expressions[combination.result] = combination
-                            n_found += 1
+                        if combination.result >= lower_bound and combination.result <= upper_bound:
+                            # if the number hasn't been found yet, add it to the found expressions
+                            if combination.result not in found_expressions:
+                                expressions_of_length[result_length].append(combination)
+                                found_expressions[combination.result] = combination
+                                n_found += 1
+                            # if the number has been found, update it if the new expression uses less allowed numbers
+                            elif len(combination.allowed_numbers_used) < len(found_expressions[combination.result].allowed_numbers_used):
+                                found_expressions[combination.result] = combination
+                            
         result_length += 1
     return found_expressions
 
